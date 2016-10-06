02a01f279d09450eb426b96876a0cdb2043e05f9
jchua1 <jchua@stuy.edu>
2016-10-06 15:34:50 -0400

    Almost done

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect
 import hashlib
 from utils import auth
 
 app = Flask(__name__)
+app.secret_key = "asdf"
 
 @app.route("/")
 @app.route("/login/")
 def login():
+    if "user" in session:
+        return redirect(url_for("welcome"))
     return render_template("login.html")
-
-@app.route("/authenticate/", methods = ["POST"])
-def authenticate():
-    username = request.form["username"]
-    password = request.form["password"]
-    m = auth.authenticate(username, password)
-    return render_template("auth.html", message = m)
 
 @app.route("/register/", methods = ["POST"])
 def reg():
@@ -22,6 +18,20 @@ def reg():
     password = request.form["password"]
     m = auth.register(username, password)
     return render_template("auth.html", message = m)
+
+@app.route("/authenticate/", methods = ["POST"])
+def authenticate():
+    username = request.form["username"]
+    password = request.form["password"]
+    m = auth.authenticate(username, password)
+    if (m == "Login successful."):
+        print "asdf"
+        session["user"] = username
+    return render_template("auth.html", message = m)
+
+@app.route("/welcome/")
+def welcome():
+    return render_template("welcome.html", name = session["user"])
 
 if __name__ == "__main__":
     app.debug = True
